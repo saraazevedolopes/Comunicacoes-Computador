cb345609b3f4b1b61d8787716d0c825b455249a5
XubunCORE 7.5 <saralopes3001@gmail.com>
2024-10-21 19:57:33 +0100

    Múltiplas tarefas a serem enviadas

--- tcp_server.py
+++ tcp_server.py
@@ -8,13 +8,15 @@ def start(address: str, port: int):
 
     s.listen(5) # ficar à escuta de pedidos de ligação (SYN), 5 - número de falhas até rejeitar a ligação, proteger de DDOS
     print("succeeded in opening")
-    connection, agent_address = s.accept() # quando aceito uma ligação de um endereço, agent_address, recebo um socket (socket dentro de um socket para comunicação) 
-
-    mensagem = connection.recv(1024) # buffer de 1024 bytes para receber mensagem (array de bytes)
-    # recvfrom é bloqueante, precisamos de threads
-
-    print(mensagem.decode())
-    print(agent_address)
     
+    while True: 
+        connection, agent_address = s.accept() # quando aceito uma ligação de um endereço, agent_address, recebo um socket (socket dentro de um socket para comunicação) 
+
+        mensagem = connection.recv(1024) # buffer de 1024 bytes para receber mensagem (array de bytes)
+        # recvfrom é bloqueante, precisamos de threads
+
+        print(mensagem.decode())
+        print(agent_address)
+        
     
 
--- udp_server.py
+++ udp_server.py
@@ -6,11 +6,13 @@ def start(address: str, port: int):
     #AF_INET diz que é ipv4
     #SOCK_DGRAM diz que é UDP
     s.bind((address, port)) # associei o socket a um par endereço-porta
-    mensagem = s.recvfrom(1024) # buffer de 1024 bytes para receber mensagem (array de bytes)
     
-    print(mensagem[0].decode()) # 0 - array de bytes que contém a mensagem
-    print(mensagem[1]) # 1 - tuplo, endereço-porta do agente
+    while True:  
+        mensagem = s.recvfrom(1024) # buffer de 1024 bytes para receber mensagem (array de bytes)
+        
+        print(mensagem[0].decode()) # 0 - array de bytes que contém a mensagem
+        print(mensagem[1]) # 1 - tuplo, endereço-porta do agente
 
-    with open("agent63.json", "r") as file_json: # with garante que há um close do open
-        data = json.load(file_json)
-        print(data)
+        with open("agent63.json", "r") as file_json: # with garante que há um close do open
+            data = json.load(file_json)
+            print(data)
