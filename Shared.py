0e699d58bb0a69302405bced36d14cd7f2bedbec
XubunCORE 7.5 <saralopes3001@gmail.com>
2024-11-17 17:04:39 +0000

    Primeira versão do parsing de tarefas

--- Shared.py
+++ Shared.py
@@ -1,10 +1,16 @@
 import threading
+from datetime import datetime
 
 class Shared:
     def __init__(self):
-        self.seq = 3
+        self.seq = 0
         self.ack = 0
         self.lock = threading.RLock()
+        self.received_condition = threading.Condition(lock=self.lock)
+        self.repeated_acks = 0
+        self.received_ack = False
+        self.received_packet = False
+        self.ack_lock = threading.RLock()
 
     def print(self):
         print(f"Dados deste agente: seq={self.seq}, ack={self.ack}")
@@ -14,6 +20,47 @@ class Shared:
 
     def release_lock(self):
         self.lock.release()
+
+    def acquire_ack_condition(self):
+        self.ack_condition.wait()
+
+    def notify_ack_condition(self):
+        self.ack_condition.notifyAll()
+    
+    def acquire_received_condition(self, seq : int) -> bool: # false -> timeout
+        timestamp = datetime.now()
+        difference = 0
+
+        try:
+            self.lock.acquire()
+            while self.ack < seq and difference < 5 : # timeout = 5s
+                self.received_condition.wait()
+                difference = (datetime.now() - timestamp).total_seconds()
+                print(f"A difference é {difference}")
+
+            print(f"Saí do loop e a diferença é {difference}")
+            return difference < 5
+        finally:
+            self.lock.release()
+
+    def notify_received_condition(self):
+        self.lock.acquire()
+        self.received_condition.notifyAll()
+        self.lock.release()
+
+    def get_received_packet(self) -> bool:
+        try:
+            self.lock.acquire()
+            return self.received_packet
+        finally:
+            self.lock.release()
+
+    def set_received_packet(self, value : bool):
+        try:
+            self.lock.acquire()
+            self.received_packet = value
+        finally:
+            self.lock.release()
 
     def get_seq(self) -> int:
         return self.seq
@@ -25,7 +72,23 @@ class Shared:
     def get_ack(self) -> int:
         return self.ack
        
-    def inc_ack(self) -> int:
-        self.ack += 1
-        return self.ack
-        
+    def set_ack(self, ack : int):
+        self.ack = ack
+        self.repeated_acks = 0
+
+    def inc_repeats(self):
+        self.repeated_acks += 1
+
+    def set_received_ack(self, state : bool):
+        try:
+            self.ack_lock.acquire()
+            self.received_ack = state        
+        finally:
+            self.ack_lock.release()
+
+    def get_received_ack(self) -> bool:
+        try:
+            self.ack_lock.acquire()
+            return self.received_ack       
+        finally:
+            self.ack_lock.release()
--- udp_agent.py
+++ udp_agent.py
@@ -1,47 +1,161 @@
 import socket
 import parser
 import threading
+import time
+from Shared import Shared
+from pythonping import ping
+import psutil
 
-#Recebe uma mensagem UDP, desserializa o conteúdo de message[0] de bytes para um formato de dados utilizável 
-#(como uma lista ou dicionário) usando parser.parse, e imprime o resultado.
-def process(s : socket.socket, message : bytes):
-    fields : list = parser.parse(message[0])
-    print(fields)
+import netifaces
 
-def register(s : socket.socket, address: str, port: int, agent_name : str):
-    # Converter para binário com preenchimento
-    agent_id = bin(int(agent_name))[2:].zfill(5)  # 5 bits
-    message_type = bin(0)[2:].zfill(3)            # 3 bits, está a 0, porque é registo
-    seq = bin(0)[2:].zfill(8)                     # 8 bits, está a 0, porque é registo
-    
-    # Concatenar todos os binários
-    message = agent_id + message_type + seq 
+def send(s : socket.socket, address : tuple, agent_id : int, message_type : int, shared_server : Shared, retransmission : bool, *args) -> int: # args tem um id e um dicionário
+    fields : list = list()
+    fields.append(bin(agent_id)[2:].zfill(5)) # id agente
+    fields.append(bin(message_type)[2:].zfill(3)) # message type
+    fields.append(0) # arbitrário, este campo depois será preenchido com o número de sequência
 
-    # Converter a string binária para um número inteiro e depois para bytes
+    shared_server.acquire_lock() # necessário para que outras threads não escrevam no mesmo nrº de sequência
+    if not retransmission:
+        seq = shared_server.get_seq()
+    else:
+        print(f"A retransmitir o pacote de seq={args[0]}")
+        seq = args[0]
+
+    fields[2] = bin(seq)[2:].zfill(8) # número de sequência
+
+    message : str = ''
+
+    for field in fields:
+        message += field
+
     message_bytes = int(message, 2).to_bytes(len(message) // 8, byteorder='big')
 
-    print(f"Mensagem binária enviada: {message}")
-    print(f"Bytes enviados: {message_bytes}")
+    if (not retransmission) and (message_type != 1): # se não é uma retransmissão, nem um Ack, então aumenta o seq
+        shared_server.inc_seq(len(message_bytes))
 
-    # Enviar a mensagem concatenada no socket
-    s.sendto(message_bytes, (address, port))
+    shared_server.release_lock()
+    # Enviar mensagem pelo socket UDP
+    s.sendto(message_bytes, address)
 
-    message = s.recvfrom(1024) # TODO era melhor ter um while para saber até onde ler, mas por agora assumir tamanho fixo
-    print(f"Recebi resposta com {message}, atualmente é um ACK")
+    return seq
+
+# só recebe tasks e acks do servidor
+#Recebe uma mensagem UDP, desserializa o conteúdo de message[0] de bytes para um formato de dados utilizável 
+#(como uma lista ou dicionário) usando parser.parse, e imprime o resultado.
+def process(s : socket.socket, message : tuple, shared_server : Shared):
+    fields : list = parser.parse(message[0])
+    print(f"Os campos são {fields}")
+
+    agent_id, message_type = fields[:2]
+    if fields[1] == 1:
+        try:
+            shared_server.acquire_lock()
+            if fields[2] > shared_server.get_ack():
+                shared_server.set_ack(fields[2])
+                shared_server.notify_received_condition()
+            else:
+                shared_server.inc_repeats()
+            shared_server.set_received_ack(True)
+        finally:
+            shared_server.release_lock()
+    else:
+        try:
+            shared_server.acquire_lock()
+            #print(f"O tamanho do pacote recebido é {len(message[0])}")
+            shared_server.inc_seq(len(message[0]))
+            #print(f"o seq neste momento é {shared_server.get_seq()}")   
+            shared_server.set_received_packet(True)
+        finally:
+            shared_server.release_lock()
+
+    if len(fields) == 8:
+        print(fields)
+        run_task(s, message[1], shared_server, fields)
+          
+def run_task(s : socket.socket, address : tuple, shared_server : Shared, fields : list):
+    print(f"args: {fields}")
+    task_id = fields[0]
+    frequency = fields[1]
+    threshold = fields[2]
+    task_type = fields[6]
+
+    if task_type == 0:
+        print("Isto é um pedido de CPU")
+        while True:
+            cpu_usage = psutil.cpu_percent()
+            print(f"Utilização do CPU: {cpu_usage}")
+            time.sleep(frequency)
+    elif task_type == 1:
+        print("Isto é um pedido de RAM")
+        while True:
+            ram_usage = psutil.virtual_memory()
+            print(f"Utilização da RAM: {ram_usage}")
+            time.sleep(frequency)
+    elif task_type == 2:
+        print("Isto é um pedido de latência")
+        destination = fields[7]
+
+        while True:
+            responses = ping(destination)
+            print(f"resultado do ping: {responses}")
+            time.sleep(frequency)
+    elif task_type == 3:
+        print("Isto é um pedido de largura de banda")
+    elif task_type == 4:
+        print("Isto é um pedido de interface")
+
+        while True:
+            for iface in interfaces:
+                iface_details = netifaces.ifaddresses(iface)
+                print(f"A interface {iface} tem esta informação: {iface_details}")
+            time.sleep(frequency)
+
+def register(s : socket.socket, address: str, port: int, agent_name : str, shared_server : Shared):
+    success = False
+    seq = send(s, (address, port), int(agent_name), 0, shared_server, False)
+    while not success:
+        success = shared_server.acquire_received_condition(seq)
+        if not success:
+            print(f"TIMEOUT no pacote de seq = {seq}")
+            send(s, (address, port), int(agent_name), 0, shared_server, True, seq)
+        else:
+            print(f"RECEBI O ACK CORRETAMENTE para seq = {seq}")
+        
+    #message = s.recvfrom(1024) # TODO era melhor ter um while para saber até onde ler, mas por agora assumir tamanho fixo
     
     # Ver número do ACK
-    process(s, message)
+    #process(s, message, shared_server)
 
+# Verifica periodicamente se há ACKs recebidos e notifica o estado partilhado
+def ack_monitor(s : socket.socket, address : tuple, agent_id : int, shared_server : Shared):
+    while True:
+        time.sleep(1)
+        shared_server.notify_received_condition()
+        if shared_server.get_received_ack():
+            print("Houve ACK recebido")
+            shared_server.set_received_ack(False)
+        
+       
+# Envia ACKs de volta ao servidor ao detectar pacotes recebidos
+def ack_sender(s : socket.socket, address : tuple, agent_id : int, shared_server : Shared):
+    while True:
+        time.sleep(1)
+        if shared_server.get_received_packet():
+            print(f"Houve pacotes recebidos e o numero de sequencia está a {shared_server.get_seq()}")
+            send(s, address, agent_id, 1, shared_server, False)
+            shared_server.set_received_packet(False)
 
 def start(address: str, port: int, agent_name : str): 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    
-    register(s, address, port, agent_name)
+    shared_server = Shared()
+    threading.Thread(target=register, args=(s, address, port, agent_name, shared_server)).start()
+    threading.Thread(target=ack_monitor, args=(s, (address, port), int(agent_name), shared_server)).start() #inicializa thread para controlar acks cumulativos
+    threading.Thread(target=ack_sender, args=(s, (address, port), int(agent_name), shared_server)).start()
     
     while True:  
         message = s.recvfrom(1024) 
         print("recebi uma mensagem UDP!")
-        threading.Thread(target=process, args=(s, message)).start() # uma thread por task
+        threading.Thread(target=process, args=(s, message, shared_server)).start() # uma thread por task
 
     s.close() 
     
